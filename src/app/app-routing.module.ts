0d02274099e9505fded2b40d5750502c49707cd6
AyGaby <gbb888021@gmail.com>
2023-11-20 08:32:09 -0300

    tendria que cambiar a react

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: BannerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
